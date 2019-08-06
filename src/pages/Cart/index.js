3f9359cf34603dc58df4196f64399a2030ba50c2
Daniel Araujo <dan.araujjo@gmail.com>
2019-08-05 22:33:40 -0300

    Estilização do Carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <div />;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PROUTOS</th>
+            <th>QUANTIDADE</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-olympikus-direct-masculino/44/D22-2835-844/D22-2835-844_detalhe1.jpg?resize=326:*"
+                alt=""
+              />
+            </td>
+            <td>
+              <strong>Tenis top</strong>
+              <span>R$ 123</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 111</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finaliza pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1111</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
