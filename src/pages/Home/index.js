704c04ed7633db877f18392dadb820ba514da7e1
Daniel Araujo <dan.araujjo@gmail.com>
2019-08-05 21:58:31 -0300

    Estilização da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-direct-masculino/44/D22-2835-844/D22-2835-844_detalhe1.jpg?resize=326:*"
+          alt=""
+        />
+        <strong>tenis</strong>
+        <span>R$</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span> ADCIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
